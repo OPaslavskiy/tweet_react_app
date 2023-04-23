e42c476b713c945df807cef4d4b311e1f65f135b
OPaslavskiy <bankir.op@gmail.com>
2023-04-23 23:35:31 +0300

    all ready with card without btn

--- src/components/TweeterCard/TweeterCard.jsx
+++ src/components/TweeterCard/TweeterCard.jsx
@@ -14,84 +14,31 @@ import {
   Button,
 } from "./TweeterCard.styled";
 
-export const TweeterCard = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    function selectUsers() {
-      getUsers()
-        .then((data) => {
-          setUsers(data);
-        })
-        .catch((err) => {});
-    }
-    selectUsers();
-  }, []);
-
-  const newUsers = JSON.parse(JSON.stringify(users));
+export const TweeterCard = ({ user, changeFollowing }) => {
   const ArrayId = JSON.parse(localStorage.getItem("ArrayId")) || [];
-
-  function updateFollowers(followers, id) {
-    const apdateUsers = newUsers.map((user) => {
-      if (user.id === id) {
-        return { ...user, followers: followers };
-      } else {
-        return user;
-      }
-    });
-    setUsers(apdateUsers);
-    const userApdete = newUsers.find((user) => user.id === id);
-    userApdete.followers = followers;
-    changeUsers(id, userApdete);
-  }
-
-  async function changeFollowing(id) {
-    const hasIdLocalStorege = ArrayId.includes(id);
-
-    if (hasIdLocalStorege) {
-      const followers = newUsers.find((user) => user.id === id).followers - 1;
-      const UpdateArrayId = ArrayId.filter((item) => item !== id);
-
-      updateFollowers(followers, id);
-      localStorage.setItem("ArrayId", JSON.stringify(UpdateArrayId));
-    } else {
-      const followers = newUsers.find((user) => user.id === id).followers + 1;
-      updateFollowers(followers, id);
-
-      ArrayId.push(id);
-      localStorage.setItem("ArrayId", JSON.stringify(ArrayId));
-    }
-  }
+  const { tweets, followers, avatar, id } = user;
   let buttonName = "";
+  const options = { style: "decimal", minimumFractionDigits: 0 };
+  const formattedFollowers = followers.toLocaleString("en-US", options);
+  buttonName = ArrayId.includes(id) ? "FOLLOW" : "FOLLOWING";
 
   return (
-    <>
-      {users.map(({ id, avatar, user, tweets, followers }) => {
-        const options = { style: "decimal", minimumFractionDigits: 0 };
-        const formattedFollowers = followers.toLocaleString("en-US", options);
-        {
-          buttonName = ArrayId.includes(id) ? "FOLLOW" : "FOLLOWING";
-        }
-        return (
-          <li key={id}>
-            <Card>
-              <Logo alt="GoIt" src={logo} />
-              <MainCardImage alt="abstract" src={mainImage} />
-              <Strip />
-              <AvatarBorder>
-                <Avatar src={avatar} width={62} height={62} alt={user} />
-              </AvatarBorder>
-              <Tweets>{tweets} TWEETS</Tweets>
+    <li>
+      <Card>
+        <Logo alt="GoIt" src={logo} />
+        <MainCardImage alt="abstract" src={mainImage} />
+        <Strip />
+        <AvatarBorder>
+          <Avatar src={avatar} width={62} height={62} alt={user.user} />
+        </AvatarBorder>
+        <Tweets>{tweets} TWEETS</Tweets>
 
-              <Followers>{formattedFollowers} FOLLOWERS</Followers>
+        <Followers>{formattedFollowers} FOLLOWERS</Followers>
 
-              <Button type="submit" onClick={() => changeFollowing(id)}>
-                {buttonName}
-              </Button>
-            </Card>
-          </li>
-        );
-      })}
-    </>
+        <Button type="submit" onClick={() => changeFollowing(id)}>
+          {buttonName}
+        </Button>
+      </Card>
+    </li>
   );
 };
--- src/components/TweeterList/TweeterList.jsx
+++ src/components/TweeterList/TweeterList.jsx
@@ -1,10 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { TweeterCard } from "../TweeterCard/TweeterCard";
 import { List } from "./TweeterList.styled";
+import { getUsers, changeUsers } from "../../services/getFetch";
 export const TweeterList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    function selectUsers() {
+      getUsers()
+        .then((data) => {
+          setUsers(data);
+        })
+        .catch((err) => {});
+    }
+    selectUsers();
+  }, []);
+
+  const newUsers = JSON.parse(JSON.stringify(users));
+  const ArrayId = JSON.parse(localStorage.getItem("ArrayId")) || [];
+
+  function updateFollowers(followers, id) {
+    const apdateUsers = newUsers.map((user) => {
+      if (user.id === id) {
+        return { ...user, followers: followers };
+      } else {
+        return user;
+      }
+    });
+    setUsers(apdateUsers);
+    const userApdete = newUsers.find((user) => user.id === id);
+    userApdete.followers = followers;
+    changeUsers(id, userApdete);
+  }
+
+  async function changeFollowing(id) {
+    const hasIdLocalStorege = ArrayId.includes(id);
+
+    if (hasIdLocalStorege) {
+      const followers = newUsers.find((user) => user.id === id).followers - 1;
+      const UpdateArrayId = ArrayId.filter((item) => item !== id);
+
+      updateFollowers(followers, id);
+      localStorage.setItem("ArrayId", JSON.stringify(UpdateArrayId));
+    } else {
+      const followers = newUsers.find((user) => user.id === id).followers + 1;
+      updateFollowers(followers, id);
+
+      ArrayId.push(id);
+      localStorage.setItem("ArrayId", JSON.stringify(ArrayId));
+    }
+  }
   return (
-    <List>
-      <TweeterCard />
-    </List>
+    <>
+      <List>
+        {users.map((user) => (
+          <TweeterCard
+            key={user.id}
+            user={user}
+            changeFollowing={changeFollowing}
+          />
+        ))}
+      </List>
+      <button type="button">Load More</button>
+    </>
   );
 };
