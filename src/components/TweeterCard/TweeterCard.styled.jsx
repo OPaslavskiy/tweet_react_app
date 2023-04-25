5da636af4e9f6dc7708207761363ffcd23322a02
OPaslavskiy <bankir.op@gmail.com>
2023-04-25 22:41:53 +0300

    add hover on the cards

--- src/components/TweeterCard/TweeterCard.styled.jsx
+++ src/components/TweeterCard/TweeterCard.styled.jsx
@@ -104,12 +104,13 @@ export const Button = styled.button`
   color: #373737;
   cursor: pointer;
 
+  &:hover,
+  &:focus {
+    color: #1976d2;
+  }
+
   box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
   border: none;
   border-radius: 10.3108px;
   margin-bottom: 36px;
-  &:hover,
-  &:focus {
-    background-color: #5cd3a8;
-  }
 `;
