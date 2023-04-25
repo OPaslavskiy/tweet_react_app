567d5ed85c1a4d07c4f136a37c04190065a228e9
OPaslavskiy <OPaslavskiy@users.noreply.github.com>
2023-04-25 20:34:47 +0000

    deploy: d2a4cf0a7f27f9879193ca429a3b83e848d60414

---
+++ assets/Home-907692bd.js
@@ -0,0 +1,27 @@
+import{s as e,N as s,j as o}from"./index-40f02624.js";const n=e(s)`
+  color: #fff;
+  font-family: "Montserrat";
+  font-style: normal;
+  font-size: 32px;
+  font-weight: bold;
+  line-height: 1.2;
+  margin-bottom: 20px;
+
+  &:hover,
+  &:focus {
+    color: #1976d2;
+  }
+`;e.div`
+  display: flex;
+  flex-direction: column;
+  font-family: "Montserrat";
+  font-style: normal;
+`;const t=e.p`
+  font-family: "Montserrat";
+  font-style: normal;
+`,r=e.a`
+  &:hover,
+  &:focus {
+    color: #1976d2;
+  }
+`,l=()=>o.jsxs("div",{style:{textAlign:"center"},children:[o.jsx(n,{to:"/tweets",children:"Welcome! Your bloggers or famous people are here."}),o.jsx(t,{style:{marginTop:"20px"},children:"I am the developer of this website."}),o.jsx(r,{href:"https://github.com/OPaslavskiy",children:"This is my page on GitHub."}),o.jsx(t,{children:"I hope you enjoy using it!"})]});export{l as default};
--- src/Layout.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Layout = styled.div`
-  padding: 64px;
-  display: flex;
-  flex-direction: column;
-  background-color: #bdc3c7;
-`;
