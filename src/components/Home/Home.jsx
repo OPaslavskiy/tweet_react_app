2983a7a787425fb12d8d97a5afa7121ae85365b8
OPaslavskiy <bankir.op@gmail.com>
2023-04-25 22:27:39 +0300

    add readMe

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,10 +4,8 @@ import { Link, P } from "./Home.styled";
 const Home = () => {
   return (
     <div style={{ textAlign: "center" }}>
-      <Link to="tweets">Your bloggers are here.</Link>
-      <P style={{ marginTop: "20px" }}>
-        I am the developer of this website. My name Oleh.
-      </P>
+      <Link to="tweets">Welcome! Your bloggers or famous people are here.</Link>
+      <P style={{ marginTop: "20px" }}>I am the developer of this website.</P>
       <a href="https://github.com/OPaslavskiy">This is my page on GitHub.</a>
       <P>I hope you enjoy using it!</P>
     </div>
